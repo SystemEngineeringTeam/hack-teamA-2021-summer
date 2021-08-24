ab4bcbe2b9a56f1dfae6bc960128e67b20b04516
shima004 <yellow030408@gmail.com>
2021-08-24 14:18:17 +0900

    add comments

--- go/api/main.go
+++ go/api/main.go
@@ -8,10 +8,19 @@ import (
 
 func main() {
 	e := echo.New()
+
+	// /coin : POST
 	e.POST("/coin", apifunc.Coin_post)
+
+	// /user : GET
 	e.GET("/user", apifunc.User_get)
+	// /user : POST
 	e.POST("/user", apifunc.User_post)
+	// /user : PUT
 	e.PUT("/user", apifunc.User_put)
+
+	// /user : DELETE
 	e.POST("/login", apifunc.Login_post)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
