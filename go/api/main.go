dd210e88bcf4bc03c17cb81be5fdd13538c7c8cf
shima004 <yellow030408@gmail.com>
2021-08-24 14:15:31 +0900

    add main frame

--- go/api/main.go
+++ go/api/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"SystemEngineeringTeam/hack-teamA-2021-summer/apifunc"
+
+	"github.com/labstack/echo"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+	e := echo.New()
+	e.POST("/coin", apifunc.Coin_post)
+	e.GET("/user", apifunc.User_get)
+	e.POST("/user", apifunc.User_post)
+	e.PUT("/user", apifunc.User_put)
+	e.POST("/login", apifunc.Login_post)
+	e.Logger.Fatal(e.Start(":1323"))
 }
