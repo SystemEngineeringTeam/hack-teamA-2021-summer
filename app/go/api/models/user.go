e162f2bedff99448b2928841298ce2c7b7ff9d6b
shima004 <yellow030408@gmail.com>
2021-08-25 18:43:27 +0900

    [add] コメントを追加した

--- app/go/api/models/user.go
+++ app/go/api/models/user.go
@@ -4,9 +4,9 @@ import "github.com/jinzhu/gorm"
 
 // フィールド名は大文字でなければ外部パッケージからアクセスできないため大文字にすること！
 type User struct {
-	gorm.Model
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Coin     int    `json:"coin"`
+	gorm.Model        // データベースで使えるようにするために必要
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+	Name       string `json:"name"`
+	Coin       int    `json:"coin"`
 }
