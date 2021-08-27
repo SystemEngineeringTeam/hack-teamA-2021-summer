d8123d147b35118f6a444b9c63cea53edea1b260
k21060 <k21060kk@aitech.ac.jp>
2021-08-27 17:53:39 +0900

    Coinの内容を変更した

--- app/go/api/apifunc/coin.go
+++ app/go/api/apifunc/coin.go
@@ -1,11 +1,26 @@
 package apifunc
 
 import (
+	"SystemEngineeringTeam/hack-teamA-2021-summer/dbfunc"
+
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type CoinPostParams struct { //
+	Email string `json:"email"`
+	Total int    `json:"total"`
+}
+
 func CoinPost(c echo.Context) error {
-	return c.String(http.StatusOK, "coin_post") // フロントに返す値
+	var params CoinPostParams
+	if err := c.Bind(&params); err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"message": "パラメータが正しくありません: " + err.Error()})
+	}
+	err := dbfunc.PostCoinInfo(params.Email, params.Total)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"message": "データベースの更新に失敗しました: " + err.Error()})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{"message": "成功しました"}) // フロントに返す値
 }
