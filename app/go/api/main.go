b4199b84e133b95d63d744663fe322bfcfb17305
shima004 <yellow030408@gmail.com>
2021-09-04 11:46:54 +0900

    [add] echo/middlewareの追加

--- app/go/api/main.go
+++ app/go/api/main.go
@@ -4,11 +4,15 @@ import (
 	"SystemEngineeringTeam/hack-teamA-2021-summer/apifunc"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	// http://localhost:8080/coin : GET apifunc->coin.go->CoinPost()
 	e.POST("/coin", apifunc.CoinPost)
