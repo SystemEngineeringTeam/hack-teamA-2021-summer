c076a27c717471d27465adf5649276db06171e3d
shima004 <yellow030408@gmail.com>
2021-09-07 18:36:38 +0900

    [add] 画像選択の画面

--- slot/src/main.js
+++ slot/src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 // yarm addでインストールしたものはimportしないと使えない
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import vuetify from './plugins/vuetify'
 Vue.config.productionTip = false
 Vue.use(VueAxios,axios)
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- slot/src/router/index.js
+++ slot/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Setting from '../views/Setting.vue'
 
 Vue.use(VueRouter)
 
@@ -23,7 +24,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
-    
+  },
+  {
+    path: '/setting',
+    name: 'Setting',
+    component: Setting
   },
 ]
 
