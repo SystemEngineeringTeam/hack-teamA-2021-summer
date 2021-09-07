fdbea5e147feb810cfaa669086c669995d80597f
shima004 <yellow030408@gmail.com>
2021-09-07 12:57:55 +0900

    [fix] ユーザモデルを返すようにした

--- app/go/api/dbfunc/coin.go
+++ app/go/api/dbfunc/coin.go
@@ -4,17 +4,17 @@ import (
 	"SystemEngineeringTeam/hack-teamA-2021-summer/models"
 )
 
-func PostCoinInfo(email string, total int) (err error) {//引数と返り値
+func PostCoin(email string, total int) (user models.User, err error) { //引数と返り値
 	db := sqlConnect()
 	defer db.Close()
 
 	// データベースからユーザーが存在するかを確認
 	var u models.User
 	if err = db.Where("email = ?", email).First(&u).Error; err != nil {
-		return err
+		return u, err
 	}
 	// データベースを更新
 	err = db.Model(models.User{}).Where("email = ?", email).Select("Coin").Updates(models.User{Coin: total}).Error
 
-	return err
+	return u, err
 }
