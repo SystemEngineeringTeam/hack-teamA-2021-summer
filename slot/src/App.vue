8a1b33d93fcd7fed4b5e152b35483e7fc3a0157e
shima004 <yellow030408@gmail.com>
2021-09-08 17:24:08 +0900

    [add] component chart.vue

--- slot/src/App.vue
+++ slot/src/App.vue
@@ -5,7 +5,6 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/login">Login</router-link> |
       <router-link to="/setting">Setting</router-link>
-
     </div>
     <router-view/>
   </div>
