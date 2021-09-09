0ac384115216329daae9c24f80f7863f932c46ff
wkympand0 <ari7ri2it1018@gmail.com>
2021-09-09 15:25:06 +0900

    homeデザイン変更

--- slot/src/App.vue
+++ slot/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/setting">Setting</router-link>
     </div>
     <router-view/>
   </div>
@@ -12,14 +8,15 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2c3e50; */
+  /* background-color:black; */
 }
 
-#nav {
+/* #nav {
   padding: 30px;
 }
 
@@ -30,5 +27,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
